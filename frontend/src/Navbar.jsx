f18accbda7920e882e48612dc4851a9f7556b183
Gowtham101202004 <gowthamprasatht.22bir@kongu.edu>
2025-01-28 14:43:13 +0530

    level2

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 // import ms_logo from "../../assets/maniyan_stores.png";
 
 function Navbar() {
@@ -23,27 +23,11 @@ function Navbar() {
             </li>
             <li>
               <NavLink className="Navlink" to="/main/order">
-                Order
-              </NavLink>
-            </li>
-            <li>
-              <NavLink className="Navlink" to="/main/account">
-                Account
+                Orders
               </NavLink>
             </li>
           </ul>
         </div>
-        {/* <div className="search_container">
-          <form onSubmit={handleSearchSubmit}>
-            <input
-              type="text"
-              placeholder="Search for Products. . ."
-              value={searchQuery}
-              onChange={handleSearchChange}
-            />
-            <FontAwesomeIcon icon={faSearch} className="search_icon" />
-          </form>
-        </div> */}
     </div>
     <Outlet/>
     </>
